f4e75119ea511ce997841d5738a78798c181c1d8
StaglianoM <stagliano.m92@gmail.com>
2024-11-24 23:05:52 +0100

    add check+error handler + not found

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const postsRouter = require("./routers/routerPost");
-
-
+const checkTime = require("./middlewares/checkTime");
+const errorHandler = require("./middlewares/errorHandler");
+const notFound = require('./middlewares/notFound');
 const app = express();
 const port = 3000;
 
+// Checktime 
+app.use(checkTime);
+
 app.use(express.static('public'));
 
-//body parses application/json
-app.use(express.json())
+// Bodyparser JSON
+app.use(express.json());
 
+// Rotte principali
 app.use("/posts", postsRouter);
 
-// Avvio il server
+// Middleware per le rotte non trovate (404)
+app.use(notFound);
+
+// Middleware per la gestione degli errori (500)
+app.use(errorHandler);
+
+// Avvio del server
 app.listen(port, () => {
     console.log(`Server in ascolto sulla porta ${port}`);
 });
